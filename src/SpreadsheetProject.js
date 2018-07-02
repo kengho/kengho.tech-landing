1cea443c338ab4c38be64611e191aec934528952
Alexander Morozov <ntcomp12@gmail.com>
2018-07-02 22:06:02 +0300

    updated spreadsheet date

--- src/SpreadsheetProject.js
+++ src/SpreadsheetProject.js
@@ -21,18 +21,16 @@ class SpreadsheetProject extends Component {
           </video>
           <div className="text-block">
             <ul>
-              <li>auto sync with server with requests queue</li>
+              <li>auto sync with server</li>
               <li>auto saving cells' values history</li>
               <li>export/import to/from CSV/JSON</li>
-              <li>basic hotkeys handlers (<code>ArrowX</code>, <code>Enter</code>, <code>Escape</code>, <code>F2</code>, <code>PageDown</code>, <code>PageUp</code>, <code>Home</code>, <code>End</code>, <code>Delete</code>)</li>
+              <li>basic hotkeys handlers (<code>ArrowX</code>, <code>Enter</code>, <code>Escape</code>, <code>F2</code>, <code>PageDown</code>, <code>PageUp</code>, <code>Delete</code>)</li>
               <li>copy/cut/paste cells (<code>Ctrl+C</code>, <code>Ctrl+X</code>, <code>Ctrl+V</code>)</li>
               <li><a href="https://github.com/omnidan/redux-undo">redux-undo</a>-powered undo/redo (<code>Ctrl+Z</code>, <code>Ctrl+Y</code>)</li>
               <li>multiline text (<code>Ctrl+Enter</code>)</li>
-              <li>autosizing cells</li>
-              <li>delete/insert row/column buttons</li>
-              <li>automatically expanding table</li>
+              <li>infinite 2D grid</li>
               <li>built-in invisible Google reCAPTCHA</li>
-              <li>clean material-ish design</li>
+              <li>clean material design</li>
             </ul>
             <div className="links-block">
               <a href="https://kengho.tech/spreadsheet/">
